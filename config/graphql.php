70130987df3499a1185931f1afe4d3c61ade7141
ramesh <ramesh@one.fit>
2020-11-23 11:21:50 +0100

    modify the env name to more meaningful one

--- config/graphql.php
+++ config/graphql.php
@@ -4,7 +4,7 @@ return [
   'graphql' => [
       'url' => env('GRAPHQL_url'),
       'connect_timeout' => env('GRAPHQL_CONNECTION_TIMEOUT',0),
-      'timeout' => env('GRAPHQL_TIMEOUT',0),
+      'timeout' => env('GRAPHQL_REQUEST_TIMEOUT',0),
   ],
 ];
 
